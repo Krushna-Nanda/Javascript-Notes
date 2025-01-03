c7de8e53648c0136669a9f6380e9de7061cc8480
Krushna Chandra Nanda <139350558+Krushna-Nanda@users.noreply.github.com>
2025-01-03 20:06:38 +0530

    Update normal_loops.js

--- 03_Basics/01_Controlflow/normal_loops.js
+++ 03_Basics/01_Controlflow/normal_loops.js
@@ -1,3 +1,46 @@
+
+function RandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+let no = RandomInt(1,10);
+let noenterbyuser;
+
+do {
+     noenterbyuser = prompt("Enter a number:");
+
+    // Convert user input to a number
+    noenterbyuser = Number(noenterbyuser);
+
+    if (noenterbyuser === no) {
+        alert("Yay! Buddy, your number matched!");
+        break;
+    }
+} while (noenterbyuser != no);
+
+
+// function addNumber(...nos) {
+//   let sum = 0;
+//   for (let index = 0; index < nos.length; index++) {
+
+//     sum += nos[index];
+
+//   }
+
+//   return sum;
+
+
+// }
+
+
+// let sum = addNumber(1, 2, 3, 4);
+
+
+// console.log(sum);
+
+
+
+
 // for loop
 
 // const arr = [1, 2, 3, 4, 5];
