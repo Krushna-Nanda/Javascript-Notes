ce650cffd79ccbde5a6b0342ea88e9dbc400afec
Krushna Chandra Nanda <139350558+Krushna-Nanda@users.noreply.github.com>
2025-02-20 23:49:17 +0530

    Update filter_map_reduce.js

--- 03_Basics/02_Advance_loop/filter_map_reduce.js
+++ 03_Basics/02_Advance_loop/filter_map_reduce.js
@@ -1,5 +1,50 @@
 /*
 
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  // forreach
+
+  // books.forEach((element) => console.log(element.title +"," + element . genre));
+
+  //map
+
+  // let newarr = books.map(funky)
+
+  // function funky(currentvalue){
+  //   if(currentvalue.genre === "Fiction"){
+  //     return currentvalue;
+  //   }
+  //   else{
+  //     return "not match";
+  //   }
+   
+  // }
+
+  // console.log(newarr);
+  
+
+ // filter
+
+ let newarr = books.filter((books) => books.genre === "Non-Fiction")
+ console.log(newarr);
+ 
+
+*/
+
+
+
+/*
+
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //filter function is return the array of filtered values.
